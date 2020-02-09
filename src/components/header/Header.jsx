3422ae7b43ed4d7149cc4e534f8f136ba0da7859
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2020-02-09 20:30:23 +0300

    initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = props => {
+    return (
+        <header className='app-header'>
+            <div className='app-header__inner'>
+                
+            </div>
+        </header>
+    )
+}
+
+export default Header
